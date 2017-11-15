6a148dddf4a865697a9a3add75506d4de759eb7a
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-15 14:50:45 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch, Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+// Import all UI Component
+import Signup from '../imports/ui/Signup';
+import Link from '../imports/ui/Link';
+import Login from '../imports/ui/Login';
+import NoMatch from '../imports/ui/Notfound';
+
+const browserHistory = createHistory();
+
+const routes = (
+  <Router history={browserHistory}>
+      <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/signup" component={Signup}/>
+          <Route path="/links" component={Link}/>
+          <Route path="*" component={NoMatch}/>
+      </Switch>
+  </Router>
+);
+
+Meteor.startup( () => {
+    ReactDOM.render(routes, document.querySelector('.app'))
+});
