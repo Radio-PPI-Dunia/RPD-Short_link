3c90762db4ff7efbe8a32bf33783369019024f76
irhamputra <irhamputraprasetyo@gmail.com>
2017-11-15 15:15:51 +0100

    add twbs & header component

--- client/main.js
+++ client/main.js
@@ -1,28 +1,32 @@
-import { Meteor } from 'meteor/meteor';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Switch, Router } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import {Meteor} from "meteor/meteor";
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import {Route, Switch, Router} from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
 
 // Import all UI Component
-import Signup from '../imports/ui/Signup';
-import Link from '../imports/ui/Link';
-import Login from '../imports/ui/Login';
-import NoMatch from '../imports/ui/Notfound';
+import Header from "../imports/ui/Header";
+import Signup from "../imports/ui/Signup";
+import Link from "../imports/ui/Link";
+import Login from "../imports/ui/Login";
+import NoMatch from "../imports/ui/Notfound";
 
 const browserHistory = createHistory();
 
 const routes = (
-  <Router history={browserHistory}>
-      <Switch>
-          <Route exact path="/" component={Login}/>
-          <Route path="/signup" component={Signup}/>
-          <Route path="/links" component={Link}/>
-          <Route path="*" component={NoMatch}/>
-      </Switch>
-  </Router>
+    <Router history={browserHistory}>
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={Login}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/links" component={Link}/>
+                <Route path="*" component={NoMatch}/>
+            </Switch>
+        </div>
+    </Router>
 );
 
-Meteor.startup( () => {
+Meteor.startup(() => {
     ReactDOM.render(routes, document.querySelector('.app'))
 });
