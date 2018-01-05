3e829f9e96d8ab9f2aa7375251d3e47f7b522669
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-05 09:57:07 +0100

    changing account base into meteor modules

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 
 export default class Link extends Component {
