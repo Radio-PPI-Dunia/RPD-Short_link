37d15286e29a4a371115f8f81ebb07716a26dc11
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-08 11:48:05 +0100

    middleware WebApp connect

--- server/main.js
+++ server/main.js
@@ -1,9 +1,21 @@
 import {Meteor} from "meteor/meteor";
-
-import '../imports/api/users';
-import '../imports/api/links';
-import '../imports/startup/simple-schema-config';
+import {WebApp} from "meteor/webapp";
+import "../imports/api/users";
+import {Links} from "../imports/api/links";
+import "../imports/startup/simple-schema-config";
 
 Meteor.startup(() => {
+    // Middleware
+    WebApp.connectHandlers.use((req, res, next) => {
+        const _id = req.url.slice(1);
+        const link = Links.findOne({_id});
 
+        if (link) {
+            res.statusCode = 302;
+            res.setHeader('Location', link.url);
+            res.end();
+        } else {
+            next();
+        }
+    });
 });
